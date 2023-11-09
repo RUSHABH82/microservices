a874064321189649feb3c71ae20428a6ecc8a1ec
RUSHABH82 <59302177+RUSHABH82@users.noreply.github.com>
2023-11-09 21:32:27 +0530

    Initial commit

---
+++ clients/src/test/java/com/rushabh/microservices/clients/ClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushabh.microservices.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
